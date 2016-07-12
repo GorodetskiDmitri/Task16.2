70e0d13b707728503ccb1cb83b9cccd118108342
Gorodetski Dmitri <gorodok.by@tut.by>
2016-07-12 21:08:15 +0300

    1

--- src/by/epam/multithreading/philosophers/Fork.java
+++ src/by/epam/multithreading/philosophers/Fork.java
@@ -2,15 +2,16 @@ package by.epam.multithreading.philosophers;
 
 public class Fork {
 	private int id;
-	
+	private int id2;
+
 	public Fork(int id) {
 		this.id = id;
 	}
-	
+
 	public int getForkId() {
 		return id;
 	}
-	
+
 	public void setForkId(int id) {
 		this.id = id;
 	}
